50944d18e353e4fa4cc3f48dc7f79dc2e14808a1
Nosakhare Emmanuel <nonreactiv@gmail.com>
2018-03-25 23:36:32 +0100

    Get only 50 records

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -14,13 +14,13 @@ class Stringer < Sinatra::Base
   get "/api/stories" do
     content_type :json
     
-    Story.newest_first.to_json(except: [:body])
+    Story.newest_first.to_json(except: [:body]).take(50)
   end
   
   get "/api/stories/:id/comments" do
     content_type :json
 
-    comments = Comment.where(story_id: params[:id]).newest_first.to_json(methods: [:user])
+    comments = Comment.where(story_id: params[:id]).newest_first.to_json(methods: [:user]).take(50)
   end
   
   post "/api/stories/:id/comments" do
